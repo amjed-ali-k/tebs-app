52d490192dbb0367670d65eeada39c0fd301260e
Amju <amjedmgm@gmail.com>
2023-06-21 10:21:31 +0530

    fix: add sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,6 @@
-import {
-  // Import `SplashScreen` from `expo-router` instead of `expo-splash-screen`
-  SplashScreen,
-
-  // This example uses a basic Layout component, but you can use any Layout.
-  Slot,
-} from "expo-router";
-
+import { SplashScreen, Slot } from "expo-router";
+import * as Sentry from "sentry-expo";
+import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import {
   useFonts,
   Inter_500Medium,
@@ -18,6 +13,13 @@ export const unstable_settings = {
   // Ensure any route can link back to `/`
   initialRouteName: "home",
 };
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
 export default function Layout() {
   // Load the font `Inter_500Medium`
   const [fontsLoaded] = useFonts({
